1f1a18413588d362666a7d01549cbbf3e21a090a
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2022-12-11 17:18:21 +0800

    Update Step-01_Processdata.R

--- Step-01_Processdata.R
+++ Step-01_Processdata.R
@@ -13,7 +13,7 @@ tcga_luad <- read.table(paste0(pathRead, "luad-rsem-count-tcga-t.txt.gz"),
 )
 colnames(tcga_luad) <- substr(colnames(tcga_luad), 1, 15)
 tcga_luad <- tcga_luad[, -1]
-save.file(tcga_luad, fileName = "TCGA-LUAD.Rdata", pathWay = "../Results/")
+save.file(tcga_luad, fileName = "TCGA-LUAD.Rdata", pathWay = pathSave)
 # tcga_luad <- readRDS(paste0(pathSave, "tcga_luad.rds"))
 
 # --------------------------------------------------
@@ -25,7 +25,7 @@ gtex_luad <- read.table(paste0(pathRead, "lung-rsem-count-gtex.txt.gz"),
 )
 colnames(gtex_luad) <- substr(colnames(gtex_luad), 1, 15)
 gtex_luad <- gtex_luad[, -1]
-check.file.exists(gtex_luad)
+save.file(gtex_luad, fileName = "GTEx-LUAD.Rdata", pathWay = pathSave)
 
 # --------------------------------------------------
 tcga_cnv <- read.table(paste0(pathRead, "all_thresholded.by_genes_whitelisted.tsv.gz"),
@@ -40,7 +40,7 @@ tcga_cnv <- tcga_cnv[
      intersect(rownames(tcga_luad), rownames(tcga_cnv)),
      intersect(colnames(tcga_luad), colnames(tcga_cnv))
 ]
-check.file.exists(tcga_cnv)
+save.file(tcga_cnv, fileName = "CNV-LUAD.Rdata", pathWay = pathSave)
 
 # --------------------------------------------------
 peak_all <- read.table(paste0(pathRead, "TCGA_ATAC_peak_Log2Counts_dedup_sample.gz"),
@@ -57,7 +57,7 @@ peak_luad <- peak_all[
 check.file.exists(peak_luad)
 
 tcga_luad_peak_samples <- tcga_luad[, which(colnames(tcga_luad) %in% colnames(peak_luad))]
-check.file.exists(tcga_luad_peak_samples)
+save.file(tcga_luad_peak_samples, fileName = "Peak-LUAD.Rdata", pathWay = pathSave)
 
 # --------------------------------------------------
 genes_adj_peak <- read.table(paste0(pathRead, "TCGA_ATAC_peak.all.probeMap.gz"),
@@ -67,7 +67,7 @@ genes_adj_peak <- read.table(paste0(pathRead, "TCGA_ATAC_peak.all.probeMap.gz"),
      sep = "\t"
 )
 genes_adj_peak <- genes_adj_peak[grep(pattern = "LUAD", rownames(genes_adj_peak)), ]
-check.file.exists(genes_adj_peak)
+save.file(genes_adj_peak, fileName = "ATAC-LUAD.Rdata", pathWay = pathSave)
 
 # --------------------------------------------------
 gtf <- rtracklayer::import(paste0(pathRead, "hg38.ncbiRefSeq.gtf.gz"))
@@ -83,6 +83,7 @@ geneinfo_df <- dplyr::select(gtf_df, c(
      "transcript_id",
      "gene_name"
 ))
-check.file.exists(geneinfo_df)
+check.file.exists()
+save.file(geneinfo_df, fileName = "Geneinfo_df.Rdata", pathWay = pathSave)
 
-survival.data(cancerType = "luad_tcga", immuneGene = "IL2") # Immune genes (accept genes list) need to be selected according to analysis!
+# survival.data(cancerType = "luad_tcga", immuneGene = "IL2") # Immune genes (accept genes list) need to be selected according to analysis!
