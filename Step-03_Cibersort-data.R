fa218c46aabfcc86045a515233511b62f772d594
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-01-14 21:42:57 +0800

    Update Step-03_Cibersort-data.R

--- Step-03_Cibersort-data.R
+++ Step-03_Cibersort-data.R
@@ -3,10 +3,10 @@
 rm(list = ls())
 
 tcga_raw <- read.table("data/luad-rsem-fpkm-tcga-t_normlized.txt.gz",
-  header = T,
-  sep = "\t",
-  row.names = 1,
-  check.names = FALSE
+                       header = T,
+                       sep = "\t",
+                       row.names = 1,
+                       check.names = FALSE
 )
 tcga_raw <- tcga_raw[, -1]
 names(tcga_raw) <- substr(colnames(tcga_raw), 1, 15)
@@ -18,10 +18,10 @@ write.table(raw_tcga, "data/luad-rsem-TPM-tcga-t_normlized.txt", quote = F, sep 
 save(raw_tcga, file = "data/luad-rsem-TPM-tcga-t_normlized.Rdata")
 # GTEx
 gtex_raw <- read.table("data/lung-rsem-fpkm-gtex_normlized.txt.gz",
-  header = T,
-  sep = "\t",
-  row.names = 1,
-  check.names = FALSE
+                       header = T,
+                       sep = "\t",
+                       row.names = 1,
+                       check.names = FALSE
 )
 gtex_raw <- gtex_raw[, -1]
 names(gtex_raw) <- substr(colnames(gtex_raw), 1, 15)
@@ -35,9 +35,9 @@ tcga_luad <- readRDS(paste0(path_save, "tcga_luad.rds"))
 gtex_luad <- readRDS(paste0(path_save, "gtex.rds"))
 
 LM22_2.0_gene <- read.table("data/LM22_2.0_gene.txt",
-  header = T,
-  row.names = 1,
-  sep = "\t"
+                            header = T,
+                            row.names = 1,
+                            sep = "\t"
 )
 
 boole_value <- row.names(raw_tcga) %in% row.names(LM22_2.0_gene)
@@ -48,9 +48,9 @@ raw_tcga <- na.omit(raw_tcga)
 raw_gtex <- raw_gtex[which(boole_value), ]
 raw_gtex <- na.omit(raw_gtex)
 sample_label <- read.table("../results/2191/NMF/cluster-nk-ligands-k=8/sample_cluster_4.csv",
-  header = F,
-  sep = ",",
-  check.names = FALSE
+                           header = F,
+                           sep = ",",
+                           check.names = FALSE
 )
 cluster1 <- sample_label[which(sample_label$V2 == 1), 1]
 cluster2 <- sample_label[which(sample_label$V2 == 2), 1]
@@ -69,9 +69,9 @@ Cibersort_data_tcga_gtex <- cbind.data.frame(
   raw_gtex
 )
 write.table(Cibersort_data_tcga_gtex, "data/Cibersort_data_ALL.txt",
-  sep = "\t",
-  quote = F,
-  row.names = T
+            sep = "\t",
+            quote = F,
+            row.names = T
 )
 # -----------------------------------------------------------------------------
 tcga_cluster1_t <- as.data.frame(t(tcga_cluster1))
@@ -100,22 +100,22 @@ data_ALL_group <- cbind.data.frame(
   raw_gtex_g
 )
 write.table(data_ALL_group, "data/data_ALL_group.txt",
-  sep = "\t",
-  quote = F,
-  row.names = T
+            sep = "\t",
+            quote = F,
+            row.names = T
 )
 # -----------------------------------------------------------------------------
 sample_label_20 <- read.table("data/sample_20.csv",
-  sep = ",",
-  header = T,
-  row.names = 1
+                              sep = ",",
+                              header = T,
+                              row.names = 1
 )
 
 sample_boole <- colnames(raw_tcga) %in% row.names(sample_label_20)
 data_20samples <- raw_tcga[, which(sample_boole)]
 data_20samples <- data_20samples[, -14]
 write.table(data_20samples, "data/Cibersort_data_20samples.txt",
-  sep = "\t",
-  quote = F,
-  row.names = T
+            sep = "\t",
+            quote = F,
+            row.names = T
 )
