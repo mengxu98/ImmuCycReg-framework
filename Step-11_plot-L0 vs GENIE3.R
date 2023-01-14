2eaebefc9985a07510442b52a69113d1306cd276
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-01-14 23:47:00 +0800

    Update Step-11_plot-L0 vs GENIE3.R

--- Step-11_plot-L0 vs GENIE3.R
+++ Step-11_plot-L0 vs GENIE3.R
@@ -24,8 +24,8 @@ p2 <- ggplot(df_res10, aes(x = Dataset, y = AUROC, fill = Method)) +
   scale_fill_manual(values = mycol) +
   # geom_errorbar(aes(ymin=AUROC - Sd, ymax=AUROC + Sd), position = position_dodge(.6), width=.2)
   scale_x_discrete(labels = c("Net1", "Net2", "Net3", "Net4", "Net5", "Net6", "Net7", "Net8", "Net9", "Net10")) +
-  theme_bw() +
-  ylim(0, 1)
+  theme_bw() # +
+  # ylim(0, 1)
 
 results_5nets <- read.csv("evaluation_gnw_5_2.csv")
 results_5nets <- results_5nets[, -1]
@@ -40,8 +40,8 @@ p1 <- ggplot(df_res5, aes(x = Dataset, y = AUROC, fill = Method)) +
   geom_bar(stat = "identity", position = position_dodge(), color = "black", width = .6) +
   scale_fill_manual(values = mycol) +
   # geom_errorbar(aes(ymin=AUROC - Sd, ymax=AUROC + Sd), position = position_dodge(.6), width=.2)
-  theme_bw() +
-  ylim(0, 1)
+  theme_bw() # +
+# ylim(0, 1)
 
 p1 + p2 + plot_layout(widths = c(1, 2)) +
   plot_annotation(tag_levels = "a") +
