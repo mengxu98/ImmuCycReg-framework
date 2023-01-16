48c205602d408315603d2d47e7857f1e5b8680bc
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-01-16 22:06:07 +0800

    Update Step-03_Cibersort.R

--- Step-03_Cibersort.R
+++ Step-03_Cibersort.R
@@ -1,4 +1,4 @@
 
 
 source("Cibersort.R")
-cibersort_all <- CIBERSORT_all("data/LM22_2.0.txt", "data/Cibersort_data_ALL.txt", perm = 1000, QN = F)
+cibersort_all <- CIBERSORT("data/LM22_2.0.txt", "data/Cibersort_data_ALL.txt", perm = 1000, QN = F)
