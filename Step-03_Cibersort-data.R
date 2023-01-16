8cf6c8007c2e31f125d9e4c6679a96676ca7496a
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-01-16 22:34:58 +0800

    Update Step-03_Cibersort-data.R

--- Step-03_Cibersort-data.R
+++ Step-03_Cibersort-data.R
@@ -1,7 +1,12 @@
 
 
 rm(list = ls())
+source("Functions.R")
+library("tidyr")
+pathRead <- "data/"
+pathSave <- "../Results/"
 
+# TCGA --------------------------------------------------------------------
 tcga_raw <- read.table("data/luad-rsem-fpkm-tcga-t_normlized.txt.gz",
                        header = T,
                        sep = "\t",
@@ -10,13 +15,11 @@ tcga_raw <- read.table("data/luad-rsem-fpkm-tcga-t_normlized.txt.gz",
 )
 tcga_raw <- tcga_raw[, -1]
 names(tcga_raw) <- substr(colnames(tcga_raw), 1, 15)
-raw_tcga <- apply(tcga_raw, 2, fpkmToTpm)
-raw_tcga[1:3, ]
-colSums(raw_tcga)
-raw_tcga <- as.data.frame(raw_tcga)
-write.table(raw_tcga, "data/luad-rsem-TPM-tcga-t_normlized.txt", quote = F, sep = "\t")
-save(raw_tcga, file = "data/luad-rsem-TPM-tcga-t_normlized.Rdata")
-# GTEx
+raw_tcga <- apply(tcga_raw, 2, fpkmToTpm) %>% as.data.frame()
+write.table(raw_tcga, "../Results/TCGA-LUAD-TPM_normlized.txt", sep = "\t", quote = F)
+save(raw_tcga, file = "../Results/TCGA-LUAD-TPM_normlized.Rdata")
+
+# GTEx --------------------------------------------------------------------
 gtex_raw <- read.table("data/lung-rsem-fpkm-gtex_normlized.txt.gz",
                        header = T,
                        sep = "\t",
@@ -29,25 +32,22 @@ raw_gtex <- apply(gtex_raw, 2, fpkmToTpm)
 raw_gtex[1:3, ]
 colSums(raw_gtex)
 raw_gtex <- as.data.frame(raw_gtex)
-save(raw_gtex, file = "data/lung-rsem-TPM-gtex_normlized.Rdata")
-#------------------------------------------------------------------------------#
-tcga_luad <- readRDS(paste0(path_save, "tcga_luad.rds"))
-gtex_luad <- readRDS(paste0(path_save, "gtex.rds"))
-
-LM22_2.0_gene <- read.table("data/LM22_2.0_gene.txt",
+write.table(raw_gtex, "../Results/GTEx-LUAD-TPM_normlized.txt", sep = "\t", quote = F)
+save(raw_gtex, file = "../Results/GTEx-LUAD-TPM_normlized.Rdata")
+# ------------------------------------------------------------------------------#
+load(paste0(pathSave, "TCGA-LUAD.Rdata"))
+load(paste0(pathSave, "GTEx-LUAD.Rdata"))
+LM22_2.0_gene <- read.table("data/LM22_2.0.txt",
                             header = T,
                             row.names = 1,
                             sep = "\t"
 )
 
-boole_value <- row.names(raw_tcga) %in% row.names(LM22_2.0_gene)
-summary(boole_value)
-data_derived <- raw_tcga[which(boole_value), ]
-raw_tcga <- raw_tcga[which(boole_value), ]
+raw_tcga <- tcga_luad[row.names(LM22_2.0_gene), ]
 raw_tcga <- na.omit(raw_tcga)
-raw_gtex <- raw_gtex[which(boole_value), ]
+raw_gtex <- gtex_luad[row.names(LM22_2.0_gene), ]
 raw_gtex <- na.omit(raw_gtex)
-sample_label <- read.table("../results/2191/NMF/cluster-nk-ligands-k=8/sample_cluster_4.csv",
+sample_label <- read.table("data/sample_cluster_4.csv",
                            header = F,
                            sep = ",",
                            check.names = FALSE
@@ -68,7 +68,7 @@ Cibersort_data_tcga_gtex <- cbind.data.frame(
   tcga_cluster4,
   raw_gtex
 )
-write.table(Cibersort_data_tcga_gtex, "data/Cibersort_data_ALL.txt",
+write.table(Cibersort_data_tcga_gtex, "../Results/Cibersort_data_ALL.txt",
             sep = "\t",
             quote = F,
             row.names = T
@@ -92,29 +92,14 @@ tcga_cluster3_g <- as.data.frame(t(tcga_cluster3_t))
 tcga_cluster4_g <- as.data.frame(t(tcga_cluster4_t))
 raw_gtex_g <- as.data.frame(t(raw_gtex_t))
 # -----------------------------------------------------------------------------
-data_ALL_group <- cbind.data.frame(
-  tcga_cluster1_g,
-  tcga_cluster2_g,
-  tcga_cluster3_g,
-  tcga_cluster4_g,
-  raw_gtex_g
+data_ALL_group <- cbind.data.frame(tcga_cluster1_g,
+                                   tcga_cluster2_g,
+                                   tcga_cluster3_g,
+                                   tcga_cluster4_g,
+                                   raw_gtex_g
 )
-write.table(data_ALL_group, "data/data_ALL_group.txt",
-            sep = "\t",
-            quote = F,
-            row.names = T
-)
-# -----------------------------------------------------------------------------
-sample_label_20 <- read.table("data/sample_20.csv",
-                              sep = ",",
-                              header = T,
-                              row.names = 1
-)
-
-sample_boole <- colnames(raw_tcga) %in% row.names(sample_label_20)
-data_20samples <- raw_tcga[, which(sample_boole)]
-data_20samples <- data_20samples[, -14]
-write.table(data_20samples, "data/Cibersort_data_20samples.txt",
+write.table(data_ALL_group, 
+            "../Results/data_ALL_group.txt",
             sep = "\t",
             quote = F,
             row.names = T
