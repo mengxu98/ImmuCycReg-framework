9122488974d8912679263a04d05797b438a5da8e
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-08-09 17:00:05 +0800

    Update Step-11_plot-Supplementary Figure 4_bc.R

--- code/Step-11_plot-Supplementary Figure 4_bc.R
+++ code/Step-11_plot-Supplementary Figure 4_bc.R
@@ -1,47 +1,24 @@
 rm(list = ls())
 source("functions/Functions.R")
 
-reg_num_all <- read.csv("results/Edge_reg_num.csv")
+pathSave <- "../../Results/"
 
-reg_num_all_dataset <- melt(reg_num_all[, c("Gene", "ImmuCycReg_framework", "Datasets")])
-p1 <- ggplot() +
-  geom_bar(
-    data = reg_num_all_dataset,
-    aes(x = Gene, y = value, fill = variable),
-    stat = "identity", position = "dodge", width = 0.9, size = 1
-  ) +
-  theme_bw() +
-  scale_fill_manual(values = c("#363636", "#999999")) +
-  labs(x = "Gene", y = "", fill = "", color = "") +
-  theme(axis.text.x = element_text(size = rel(1), angle = 45, hjust = 1))
+netInfor <- read.csv(paste0(pathSave, "Edge_reg_num.csv"))
 
-reg_num_all_reg <- melt(reg_num_all[, c("Gene", "Negative", "Positive")])
-p2 <- ggplot() +
-  geom_bar(
-    data = reg_num_all_reg,
-    aes(x = Gene, y = value, fill = variable),
-    stat = "identity", position = "dodge", width = 0.9, size = 1
-  ) +
-  theme_bw() +
-  scale_fill_manual(values = c("#6666ff", "#ff9933")) +
-  labs(x = "Gene", y = "", fill = "", color = "") +
-  theme(axis.text.x = element_text(size = rel(1), angle = 45, hjust = 1))
+netEdgeInfor <- melt(netInfor[, c("Gene", "ImmuCycReg_framework", "Datasets")], id = "Gene")
+p1 <- bar.plot(netEdgeInfor, barColor = c("white", "gray"))
 
-p1 / p2 +
-  plot_annotation(tag_levels = "a") +
+netRegInfor <- melt(netInfor[, c("Gene", "Negative", "Positive")], id = "Gene")
+p2 <- bar.plot(netRegInfor, barColor = c("#6666ff", "#ff9933"))
+
+p3 <- p1 + p2 +
   plot_layout(ncol = 2) +
+  plot_annotation(tag_levels = "a") +
   plot_layout(guides = "collect") &
-  theme(legend.position = "bottom") +
-    theme(text = element_text(size = 12))
-theme(text = element_text(family = "Times New Roman"))
+  theme(legend.position = "bottom")
 
-ggsave(paste0("../manuscript_review/figure/", "Supplementary Figure 4_bc.png"),
-  width = 7.5,
-  height = 3,
-  dpi = 600
-)
-ggsave(paste0("../manuscript_review/figure/", "Supplementary Figure 4_bc.pdf"),
-  width = 7.5,
-  height = 3,
-  dpi = 600
-)
+ggsave(paste0(pathSave, "Figure/Supplementary Figure 4_bc.pdf"),
+       p3,
+       width = 7.5,
+       height = 3,
+       dpi = 600)
